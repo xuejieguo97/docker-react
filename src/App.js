3b8fd3283c6bc715299f736424e759e444cc1297
Xuejie Guo <xuejieguo97@gmail.com>
2023-10-05 14:39:29 -0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
+          Hello World! This is my first React App!
         </a>
       </header>
     </div>
